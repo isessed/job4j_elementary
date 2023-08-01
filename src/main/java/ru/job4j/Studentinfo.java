45a68dd66725d61697f20c046345d3ee6684ec02
Pavel <isessed@gmail.com>
2023-08-01 16:26:04 +0300

    3.3. Фиксация изменений на github [#243564]

---
+++ src/main/java/ru/job4j/Studentinfo.java
@@ -0,0 +1,9 @@
+package ru.job4j;
+
+public class Studentinfo {
+    public static void main(String[] args) {
+    System.out.println("Pavel Petrov");
+    System.out.println("09.07.2000");
+    }
+
+}
